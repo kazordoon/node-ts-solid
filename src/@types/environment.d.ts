3f5837e83e6e3d7ede7e23a31bf4714cbc855909
Felipe Barros <felipetech@tuta.io>
2020-07-30 20:50:21 -0300

    chore: Add the mail environment variables

--- src/@types/environment.d.ts
+++ src/@types/environment.d.ts
@@ -1,9 +1,13 @@
 declare global {
   namespace NodeJS {
     export interface ProcessEnv {
-      NODE_ENV: 'development' | 'test' | 'production'
-      HOST: string
-      PORT: number
+      NODE_ENV: 'development' | 'test' | 'production';
+      HOST: string;
+      PORT: number;
+      MAIL_HOST: string;
+      MAIL_PORT: number;
+      MAIL_USER: string;
+      MAIL_PASS: string;
     }
   }
 }
